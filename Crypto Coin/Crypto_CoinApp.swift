70c91cd1e5b94fe6b25f58b6ecfa91218af37190
Kelvin Ricafort <kelvs.r@outlook.com>
2024-06-17 22:13:50 +0800

    Initial Commit

---
+++ Crypto Coin/Crypto_CoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_CoinApp.swift
+//  Crypto Coin
+//
+//  Created by Kelvin on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_CoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
